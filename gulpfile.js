29e1a1a727733001e47a78986978f06546041147
Chris Targett <chris@xlevus.net>
2014-08-05 21:42:26 +0100

    Working gulp libs command

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+var gulp = require("gulp");
+
+var paths = {
+    dest: 'flask_kibble/static'
+}
+
+gulp.task("libs", function(){
+    var mainBowerFiles = require("main-bower-files");
+    var gulpFilter = require('gulp-filter');
+    var minifycss = require('gulp-minify-css');
+    var flatten = require('gulp-flatten');
+    var rename = require('gulp-rename');
+    var uglify = require('gulp-uglify');
+    var concat = require('gulp-concat');
+    var sourcemaps = require("gulp-sourcemaps");
+
+    var jsFilter = gulpFilter("*.js");
+    var cssFilter = gulpFilter("*.css");
+    var fontFilter = gulpFilter(['*.eot', '*.woff', '*.svg', '*.ttf']);
+
+    return gulp.src(mainBowerFiles())
+        .pipe(jsFilter)
+            .pipe(sourcemaps.init())
+                .pipe(concat('libs.js'))
+                .pipe(uglify({preserveComments: 'some'}))
+                .pipe(rename({suffix: '.min'}))
+            .pipe(sourcemaps.write('.'))
+            .pipe(gulp.dest(paths.dest + '/js'))
+            .pipe(jsFilter.restore())
+        .pipe(cssFilter)
+            .pipe(sourcemaps.init())
+                .pipe(concat('libs.css')) 
+            .pipe(sourcemaps.write('.'))
+            .pipe(gulp.dest(paths.dest + '/css'))
+            .pipe(cssFilter.restore())
+        .pipe(fontFilter)
+            .pipe(flatten())
+            .pipe(gulp.dest(paths.dest + '/fonts'));
+})
+
+gulp.task("default", ['libs']);
